b4089c758895a23db63b655000b4be6611633dcc
mushroom0000 <110604001+mushroom0000@users.noreply.github.com>
2023-02-16 11:35:43 +0530

    Update application.py

--- application.py
+++ application.py
@@ -139,4 +139,4 @@ def reviews():
         
         
 if __name__ == '__main__':
-    application.run(host = '0.0.0.0', port = 8000, debug = True)
+    application.run(host = '0.0.0.0', port = 8080, debug = True)
